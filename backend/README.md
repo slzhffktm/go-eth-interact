1688fb2706dbf60a4f50f56dec921d0788765e77
slzhffktm <pinkappleberry98@gmail.com>
2022-09-03 11:26:24 +0700

    :rocket: feat: update backend code

--- backend/README.md
+++ backend/README.md
@@ -1,18 +1,15 @@
 # Running The Go Program to Interact with The Smart Contract
 
-This is following the [goethereumbook.org Smart Contracts Section](https://goethereumbook.org/en/smart-contracts/) and [Creating a simple Ethereum Smart Contract in Golang](https://towardsdev.com/creating-a-simple-ethereum-smart-contract-in-golang-138b9439f64e).
+This is following the [goethereumbook.org Smart Contract Write Section](https://goethereumbook.org/en/smart-contract-write/) and [Creating a simple Ethereum Smart Contract in Golang](https://towardsdev.com/creating-a-simple-ethereum-smart-contract-in-golang-138b9439f64e).
 
 ## Getting Started
 
 1. Make sure you have `go` installed. This tutorial is using `go 1.18`
 2. Copy the `.env.example` file to `.env` and fill in the values, 
-   where the `CONTRACT_ADDRESS` is the address from the `contracts/.env`,
-   and the `OWNER_ADDRESS` is the address that was used to deploy the smart contract
-3. Go to `main.go` and change the message at line 39 with your name 
+   where the `CONTRACT_ADDRESS` and `PRIVATE_KEY` are from the `contracts/.env`,
+3. Go to `main.go` and change the message at line 71 with your name 
   ```
-    resp2, err := instance.GreetOwner(&bind.CallOpts{
-		From: common.HexToAddress(os.Getenv("OWNER_ADDRESS")),
-	}, "<your name here>")
+    tx, err := instance.UpdateOwnerName(auth, "<your name here>")
   ```
 4. Run `make start` to start the program
-5. You should see the response from the smart contract being logged
+5. You should see the responses from the smart contract being logged
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
 	helloworld "github.com/slzhffktm/go-eth-interact/contracts"
 )
 
 func main() {
-	client, err := ethclient.Dial(os.Getenv("API_URL"))
+	ctx := context.Background()
+
+	client, err := ethclient.DialContext(ctx, os.Getenv("API_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,21 +31,58 @@ func main() {
 	}
 
 	// No owner address needed for this call.
-	resp, err := instance.Hello(nil)
+	resp, err := instance.Hello(&bind.CallOpts{
+		Context: ctx,
+	})
 	if err != nil {
 		log.Fatal(fmt.Errorf("Hello error: %w", err))
 	}
-
 	fmt.Printf("The respond from Hello() is: %s\n", resp)
 
+	privateKey, err := crypto.HexToECDSA(os.Getenv("PRIVATE_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(ctx, fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	// Owner needs to sign the transaction for calling this function.
+	// Thus auth is needed.
+	tx, err := instance.UpdateOwnerName(auth, "Owner from Go")
+	if err != nil {
+		log.Fatal(fmt.Errorf("UpdateOwnerName error: %w", err))
+	}
+	fmt.Printf("Transaction sent: %s\n", tx.Hash().Hex())
+
 	// Only the owner can call the next function.
-	// Thus we need to specify the owner address.
+	// Thus we need to specify the from address.
 	resp2, err := instance.GreetOwner(&bind.CallOpts{
-		From: common.HexToAddress(os.Getenv("OWNER_ADDRESS")),
-	}, "Owner")
+		From:    fromAddress,
+		Context: ctx,
+	})
 	if err != nil {
 		log.Fatal(fmt.Errorf("GreetOwner error: %w", err))
 	}
-
 	fmt.Printf("The respond from GreetOwner() is: %s\n", resp2)
+
 }
